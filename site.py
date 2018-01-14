c430c25b9b178d5a71dbac5f35087ec72b9dda71
Tonexus <ual_ynot@yahoo.com>
2018-01-13 19:53:24 -0800

    Added port

--- site.py
+++ site.py
@@ -6,5 +6,5 @@ app = flask.Flask(__name__)
 def hello():
     return "Hello World!"
     
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port='8080')
